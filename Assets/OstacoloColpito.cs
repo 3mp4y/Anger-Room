30527df1b16f363933afe94f90d03204a2b241a2
fulviopetrelli <fulviope7@gmail.com>
2026-05-19 10:04:17 +0200

    ostacolo sfera

--- Assets/OstacoloColpito.cs
+++ Assets/OstacoloColpito.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class OstacoloColpito : MonoBehaviour
 {
-    public Transform InizioLabirinto;
+    public Transform InizioLabirinto1;
+    public Transform InizioLabirinto2;
+    public Transform InizioLabirinto3;
+    public GameObject Giocatore;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +24,31 @@ public class OstacoloColpito : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        // Verifica il tag dell'oggetto con cui sei entrato in contatto
-        if (other.CompareTag("Ostacolo"))
+        // Se la sfera colpisce l'oggetto con il tag player:
+        if (other.CompareTag("Player"))
         {
             Debug.Log("Ostacolo colpito, torna all'inizio del labirinto!");
-            Debug.Log(InizioLabirinto.localPosition);
-            transform.position = InizioLabirinto.localPosition;
-            transform.rotation = InizioLabirinto.localRotation;
+            //calcolo le distanze tra il giocatore e gli inizi dei 3 labirinti per capire velocemente in che labirinto siamo
+            float distanza1 = Vector3.Distance(other.transform.position, InizioLabirinto1.position);
+            float distanza2 = Vector3.Distance(other.transform.position, InizioLabirinto2.position);
+            float distanza3 = Vector3.Distance(other.transform.position, InizioLabirinto3.position);
+            if(distanza1 < distanza2 && distanza1 < distanza3) //sono nel labirinto1
+            {
+                Giocatore.transform.position = InizioLabirinto1.position;
+                Giocatore.transform.rotation = InizioLabirinto1.rotation;
+            }
+            else if(distanza2 < distanza1 && distanza2 < distanza3) //sono nel labirinto2
+            {
+                Giocatore.transform.position = InizioLabirinto2.position;
+                Giocatore.transform.rotation = InizioLabirinto2.rotation;
+            }
+            else //sono nel labirinto3
+            {
+                Giocatore.transform.position = InizioLabirinto3.position;
+                Giocatore.transform.rotation = InizioLabirinto3.rotation;
+            }
 
-            
+
         }
     }
 }
